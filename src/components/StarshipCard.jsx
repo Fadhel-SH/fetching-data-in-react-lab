6c7c924a8ae8db5b6c01f0b62e2b708fa67dde6c
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-12 12:59:47 +0300

    Render starship data using StarshipList and StarshipCard components

---
+++ src/components/StarshipCard.jsx
@@ -0,0 +1,15 @@
+// src/components/StarshipCard.jsx
+
+const StarshipCard = ({ starship }) => {
+    return (
+      <li>
+        <h2>{starship.name}</h2>
+        <p>Class: {starship.starship_class}</p>
+        <p>Manufacturer: {starship.manufacturer}</p>
+        <p>Model: {starship.model}</p>
+      </li>
+    );
+  };
+  
+  export default StarshipCard;
+  
